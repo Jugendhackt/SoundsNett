4dd53d8f87760cd3aab6a2ade70e08f64d59fc6b
Chrikau123 <33779455+Chrikau123@users.noreply.github.com>
2017-11-18 22:18:12 +0100

    Progress

--- SoundsNettApp/app/src/main/java/at/jugendhackt/soundsnett/soundsnett/views/WebViewActivity.java
+++ SoundsNettApp/app/src/main/java/at/jugendhackt/soundsnett/soundsnett/views/WebViewActivity.java
@@ -6,7 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import at.jugendhackt.soundsnett.soundsnett.R;
 
@@ -21,8 +23,11 @@ public class WebViewActivity extends AppCompatActivity {
 
 
         WebView wv = findViewById(R.id.webview);
-        //wv.loadUrl("file:///android_asset/index.html");
-        wv.loadUrl("https://www.google.com");
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new WebViewClient());
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.loadUrl("file:///android_asset/index.html");
+        //wv.loadUrl("https://www.google.com");
     }
 
 }
